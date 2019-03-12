083d9c6e31448ca5d6913f66283908308a833b88
jason.wang <jason.wang@starlight-sms.com>
2019-03-12 18:10:34 +0800

    add graphql npm pack

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const Body = require('body-parser');
+const { graphqlExpress } = require('apollo-server-express');
+const {
+    GraphQLObjectType,
+    GraphQLSchema,
+    GraphQLString,
+    GraphQLInt
+} = require('graphql');
+
+
+const User = new GraphQLObjectType({
+    name: 'User',
+    description: 'User对象',
+    fields: {
+        id: {
+            type: GraphQLInt
+        },
+        name: {
+            type: GraphQLString
+        },
+    }
+});
+
+const Query = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        user: {
+            type: User,
+            args: {
+                id: {
+                    type: GraphQLInt
+                }
+            },
+            resolve: function(root, args) {
+                return { id: 1, name: '2' };
+            }
+        }
+    }
+});
+const myGraphQLSchema = new GraphQLSchema({
+    query: Query
+});
+const app = new express();
+const PORT = 3000;
+
+// Body is needed just for POST.
+app.use(Body());
+
+app.post('/graphql', graphqlExpress({ schema: myGraphQLSchema }));
+app.get('/graphql', graphqlExpress({ schema: myGraphQLSchema }));
+app.listen(PORT);
