05647f1eea3b491923a069bd7f0add586808d230
jason_home <1092783119@qq.com>
2019-03-16 12:18:57 +0800

    grqphql test

--- server.js
+++ server.js
@@ -4,23 +4,36 @@ var { buildSchema } = require('graphql');
 
 // 使用 GraphQL schema language 构造一个 schema
 var schema = buildSchema(`
+  type OutVm{
+    dice:Int
+    sides:Int
+    }
   type Query {
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    rollDice(numDice: Int!, numSides: Int):OutVm
+    quoteOfTheDay: String
+    random: Float!
+    rollThreeDice: [Int]
   }
 `);
 
 // root 为每个端点入口 API 提供一个解析器
 var root = {
     rollDice: function({ numDice, numSides }) {
-        var output = [];
-        for (var i = 0; i < numDice; i++) {
-            output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-        }
-        return output;
+        return { dice: numDice, sides: numSides };
+    },
+    quoteOfTheDay: () => {
+        return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
+    },
+    random: () => {
+        return Math.random();
+    },
+    rollThreeDice: () => {
+        return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
     }
 };
 
 var app = express();
+app.use(express.static('static'));
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     rootValue: root,
