e11fa9dcb93a8b971a4cbcf71f3304309f4b137c
jason_home <1092783119@qq.com>
2019-03-17 21:56:58 +0800

    graphql server

--- data.js
+++ data.js
@@ -4,7 +4,8 @@ var userList = [{
         firstName: 'hu',
         lastName: 'hu',
         roleId: 1,
-        createDate: "2018-11-15 14:32:43"
+        createDate: "2018-11-15 14:32:43",
+        employee: [2]
     },
     {
         id: 2,
@@ -12,7 +13,8 @@ var userList = [{
         firstName: 'ko',
         lastName: 'ko',
         roleId: 2,
-        createDate: "2018-8-15 14:32:43"
+        createDate: "2018-8-15 14:32:43",
+        leader: 1
     },
     {
         id: 3,
@@ -20,7 +22,8 @@ var userList = [{
         firstName: 'RE',
         lastName: 'fg',
         roleId: 1,
-        createDate: "2018-7-15 14:32:43"
+        createDate: "2018-7-15 14:32:43",
+        employee: [3]
     },
     {
         id: 4,
@@ -28,7 +31,7 @@ var userList = [{
         firstName: 'GF',
         lastName: 'ds',
         roleId: 2,
-        createDate: "2018-6-15 14:32:43"
+        leader: 3
     },
     {
         id: 5,
@@ -36,7 +39,8 @@ var userList = [{
         firstName: 'AS',
         lastName: 'wd',
         roleId: 1,
-        createDate: "2018-3-15 14:32:43"
+        createDate: "2018-3-15 14:32:43",
+        employee: []
     }
 ];
 
--- graphql/schema.js
+++ graphql/schema.js
@@ -9,7 +9,11 @@ var {
     GraphQLList,
     GraphQLSchema,
     GraphQLNonNull,
-    GraphQLScalarType
+    GraphQLScalarType,
+    GraphQLInterfaceType,
+    GraphQLUnionType,
+    GraphQLInputObjectType,
+
 } = require('graphql');
 
 let GraphQLDate = new GraphQLScalarType({
@@ -41,8 +45,8 @@ let Role = new GraphQLEnumType({
     }
 });
 
-let userModel = new GraphQLObjectType({
-    name: 'User',
+let userModel = new GraphQLInterfaceType({
+    name: 'UserModel',
     fields: {
         id: {
             type: GraphQLInt
@@ -62,54 +66,225 @@ let userModel = new GraphQLObjectType({
         createDate: {
             type: GraphQLDate
         }
+    },
+    resolveType: (value) => {
+        if (value.employee) {
+            return adminUser;
+        }
+        if (value.leader) {
+            return normalUser;
+        }
     }
 });
 
-let userList = new GraphQLObjectType({
-    name: 'users',
+let adminUser = new GraphQLObjectType({
+    name: "AdminUser",
+    interfaces: [userModel],
+    fields: {
+        id: {
+            type: GraphQLInt
+        },
+        name: {
+            type: GraphQLString
+        },
+        firstName: {
+            type: GraphQLString
+        },
+        lastName: {
+            type: GraphQLString
+        },
+        roleId: {
+            type: Role
+        },
+        createDate: {
+            type: GraphQLDate
+        },
+        employee: {
+            type: new GraphQLList(GraphQLInt)
+        }
+    },
+    isTypeOf: (obj) => {
+        return obj.employee;
+    }
+});
+
+let normalUser = new GraphQLObjectType({
+    name: "NormalUser",
+    interfaces: [userModel],
+    fields: {
+        id: {
+            type: GraphQLInt
+        },
+        name: {
+            type: GraphQLString
+        },
+        firstName: {
+            type: GraphQLString
+        },
+        lastName: {
+            type: GraphQLString
+        },
+        roleId: {
+            type: Role
+        },
+        createDate: {
+            type: GraphQLDate
+        },
+        leader: {
+            type: GraphQLInt
+        }
+    },
+    isTypeOf: (obj) => {
+        return obj.leader;
+    }
+
+});
+
+let userUnion = new GraphQLUnionType({
+    name: "UserUnion",
+    types: [adminUser, normalUser],
+    resolveType: (value) => {
+        if (value.employee) {
+            return adminUser;
+        }
+        if (value.leader) {
+            return normalUser;
+        }
+    }
+});
+
+let userQueryInput = new GraphQLInputObjectType({
+    name: "UserQueryInput",
+    fields: {
+        id: {
+            type: GraphQLInt
+        },
+        name: {
+            type: GraphQLString
+        }
+    }
+});
+
+let userQuery = new GraphQLObjectType({
+    name: 'UserQuery',
     fields: {
         userList: {
             type: new GraphQLList(userModel),
-            args: {},
+            args: {
+                userQuery: {
+                    type: userQueryInput
+                }
+            },
             resolve: (source, args, context, info) => {
-                return userData;
+                let result = (_.filter(userData, (x) => {
+                    return args.userQuery && args.userQuery.name ? x.name === args.userQuery.name : true
+                }));
+                return result;
             }
         },
         user: {
-            type: userModel,
+            type: userUnion,
             args: {
                 id: {
                     type: new GraphQLNonNull(GraphQLInt)
                 }
             },
             resolve: (source, args, context, info) => {
-                return _.first(userData, x => x.id == args.id);
-            }
-        },
-        addUser: {
-            type: userModel,
-            args: {
-                id: {
-                    type: new GraphQLNonNull(GraphQLInt)
-                },
-                name: {
-                    type: new GraphQLNonNull(GraphQLString)
-                },
-                roleId: {
-                    type: new GraphQLNonNull(GraphQLInt)
-                }
-            },
-            resolve: (args) => {
-
-                userData.push(args);
-                return args;
+                let result = _.first(_.filter(userData, (x) => { return x.id === args.id }));
+                return result;
             }
         }
     }
 });
 
+let addUserInput = new GraphQLInputObjectType({
+    name: "AddUserInput",
+    fields: {
+        name: {
+            type: new GraphQLNonNull(GraphQLString)
+        },
+        firstName: {
+            type: GraphQLString
+        },
+        lastName: {
+            type: GraphQLString
+        },
+        roleId: {
+            type: new GraphQLNonNull(GraphQLInt)
+        }
+    }
+});
+
+let updateUserInput = new GraphQLInputObjectType({
+    name: "UpdateUserInput",
+    fields: {
+        id: {
+            type: new GraphQLNonNull(GraphQLInt)
+        },
+        name: {
+            type: new GraphQLNonNull(GraphQLString)
+        }
+    }
+});
+
+let userMutation = new GraphQLObjectType({
+    name: "UserMutation",
+    fields: {
+        addUser: {
+            type: userUnion,
+            args: {
+                addModel: { type: addUserInput }
+            },
+            resolve: (source, args, context) => {
+                var names = _.split(args.addModel.name, '.');
+                let newUser = {
+                    id: userData.length + 1,
+                    name: args.addModel.name,
+                    roleId: args.addModel.roleId,
+                    firstName: args.addModel.firstName ? args.addModel.firstName : _.first(names),
+                    lastName: args.addModel.lastName ? args.addModel.lastName : _.last(names),
+                    createDate: new Date(),
+                }
+                if (args.addModel.roleId == 1) {
+                    newUser.employee = [];
+                } else {
+                    newUser.leader = 1;
+                }
+                userData.push(newUser);
+                return newUser;
+            }
+        },
+        removeUser: {
+            type: GraphQLInt,
+            args: {
+                id: {
+                    type: new GraphQLNonNull(GraphQLInt)
+                }
+            },
+            resolve: (source, args, context) => {
+                userData = _.filter(userData, (x) => { return x.id != args.id });
+                return 1;
+            }
+        },
+        updateUserName: {
+            type: userUnion,
+            args: {
+                updateModel: { type: updateUserInput }
+            },
+            resolve: (source, args, context) => {
+                let updateUser = _.filter(userData, (x) => { return x.id == args.updateModel.id });
+                updateUser.name = args.updateModel.name;
+                var names = _.split(args.name, '.');
+                updateUser.firstName = _.first(names);
+                updateUser.lastName = _.last(names);
+                return _.first(userData, (x) => { return x.id == args.updateModel.id });
+            }
+        }
+    }
+});
 
 module.exports =
     new GraphQLSchema({
-        query: userList
+        query: userQuery,
+        mutation: userMutation
     });
